923fede3a0cb72afcf0007af6069aaaaf05a0dfc
Mateus Fugita <mateushiwao@gmail.com>
2020-09-23 21:42:40 -0300

    add Flask app

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from flask_cors import CORS
+import pickle, os
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    entrada = request.get_json(force=True)['answers']
+
+    modelo = open('modelo_DecisionTreeClassifier','rb')
+    modelo = pickle.load(modelo)
+    kmeans = open('modelo_kmeans','rb')
+    kmeans = pickle.load(kmeans)
+
+    cluster = kmeans.predict([entrada])[0]
+    entrada.append(cluster)
+    retorno = modelo.predict([entrada])[0]
+    return { 'country': retorno }
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
